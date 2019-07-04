0cc78030e022d7d56247defbc31511ec0bcbfd01
saranya-1621 <51949644+saranya-1621@users.noreply.github.com>
2019-07-04 10:55:40 +0530

    Update 91.py

--- 91.py
+++ 91.py
@@ -1,4 +1,4 @@
-l1,l2,l3=map(int,input().split())
-sk=l1*l2*l3
-sk=2*l1*l2+2*l2*l3+2*l1*l3
-print(sh,sk,end=" ")
+ra1,ra2,ra3=map(int,input().split())
+area=(2*((ra1*ra2)+(ra2*ra3)+(ra3*ra1)))
+vol=ra1*ra2*ra3
+print(area,vol)
